cef696d360f9adfeb5955482e12aa4b104c65cd7
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-08-13 08:08:02 -0300

    feat: create screen login

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  Background,
+  Container,
+  Logo,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+  Link,
+  LinkText,
+} from './styles';
+import {Platform} from 'react-native';
+
+import {useNavigation} from '@react-navigation/native';
+
+export default function SignIn() {
+  const navigation = useNavigation();
+
+  return (
+    <Background>
+      <Container behavior={Platform.OS === 'ios' ? 'padding' : ''} enabled>
+        <Logo source={require('../../assets/Logo.png')} />
+
+        <AreaInput>
+          <Input placeholder="Seu E-mail" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Sua Senha" />
+        </AreaInput>
+
+        <SubmitButton activeOpacity={0.8}>
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+
+        <Link onPress={() => navigation.navigate('SignUp')}>
+          <LinkText>Criar uma conta!</LinkText>
+        </Link>
+      </Container>
+    </Background>
+  );
+}
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {ActivityIndicator, View} from 'react-native';
+import AuthRoutes from './auth.routes';
+
+function Routes() {
+  const loading = false;
+  const signed = false;
+  return signed ? <View /> : <AuthRoutes />;
+}
+
+export default Routes;
