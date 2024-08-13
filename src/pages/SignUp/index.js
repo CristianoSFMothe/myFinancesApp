a264ad1cb14938149468b8903d4d47a4c4b73608
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-08-13 08:18:23 -0300

    feat: create screen sign up

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {Platform} from 'react-native';
+
+import {
+  Background,
+  Container,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+} from '../SignIn/styles';
 
 export default function SignUp() {
   return (
-    <View>
-      <Text>TELA DE CADASTRO</Text>
-    </View>
+    <Background>
+      <Container behavior={Platform.OS === 'ios' ? 'padding' : ''} enabled>
+        <AreaInput>
+          <Input placeholder="Nome" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Seu e-mail" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Sua senha" />
+        </AreaInput>
+
+        <SubmitButton>
+          <SubmitText>Cadastrar</SubmitText>
+        </SubmitButton>
+      </Container>
+    </Background>
   );
 }
